2a262c61639e7f0208b560ea42c3cf24b979582b
Nicolas Filotto <essobedo@users.noreply.github.com>
2023-06-27 18:01:26 +0200

    CAMEL-19510: reactive-streams - Fix the auto configuration (#113)
    ## Motivation
    
    No routes are even started
    
    ## Modifications
    
    * Fixes the simple expression
    * The way [to register auto-configurations has changed](https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.0-Migration-Guide#auto-configuration-files), the code must be changed consequently
    * Removes unnecessary annotation `EnableAutoConfiguration` as it is already included `SpringBootApplication`
    
    (cherry picked from commit 8584e4e7fc4c40bd467366f2970c423ce0cb0ba9)

--- amqp/src/main/java/sample/camel/SampleAmqApplication.java
+++ amqp/src/main/java/sample/camel/SampleAmqApplication.java
@@ -17,11 +17,9 @@
 package sample.camel;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class SampleAmqApplication {
     public static void main(String[] args) {
         SpringApplication.run(SampleAmqApplication.class, args);
