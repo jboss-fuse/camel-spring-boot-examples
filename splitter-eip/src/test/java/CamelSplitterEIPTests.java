f4eaf0a467a2bb7ef8188cb2d76b5999768ab3c9
Tom Cunningham <tcunning@redhat.com>
2026-04-04 18:40:17 -0400

    Use junit6 in splitter-eip

--- splitter-eip/src/test/java/CamelSplitterEIPTests.java
+++ splitter-eip/src/test/java/CamelSplitterEIPTests.java
@@ -32,7 +32,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.apache.camel.test.junit5.TestSupport.assertIsInstanceOf;
+import static org.apache.camel.test.junit6.TestSupport.assertIsInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
